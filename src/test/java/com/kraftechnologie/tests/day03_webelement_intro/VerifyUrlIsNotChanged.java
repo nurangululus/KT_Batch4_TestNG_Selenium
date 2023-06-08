461b5d1c545acca0a3d2850241b9e840ade845da
Ftosun21 <tosunfatih9.2@gmail.com>
2023-06-08 20:08:20 +0300

    added verify that url is not changed

---
+++ src/test/java/com/kraftechnologie/tests/day03_webelement_intro/VerifyUrlIsNotChanged.java
@@ -0,0 +1,43 @@
+package com.kraftechnologie.tests.day03_webelement_intro;
+
+import com.kraftechnologie.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class VerifyUrlIsNotChanged {
+    public static void main(String[] args) throws InterruptedException {
+
+        /** Class Task
+         * open the chrome browser
+         * go to  https://www.krafttechexlab.com/login  page
+         * click on login button
+         * verify that url is not changed
+         */
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome"); //empty browser
+        driver.get("https://www.krafttechexlab.com/login");
+
+
+        String expectedURL= driver.getCurrentUrl();
+        System.out.println("expectedURL = " + expectedURL);
+
+        //click on login button
+        WebElement loginBtn =driver.findElement(By.cssSelector("[type=\"submit\"]"));
+        loginBtn.click();
+
+        String actualURL=driver.getCurrentUrl();
+        System.out.println("actualURL = " + actualURL);
+
+        if(expectedURL.equals(actualURL)){
+            System.out.printf("Pass");
+        }else {
+            System.out.printf("Fail");
+        }
+
+        Thread.sleep(5000);
+        driver.close();
+
+
+    }
+}
