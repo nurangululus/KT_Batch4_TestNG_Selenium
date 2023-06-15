e311423b02109ddb158cf31edd76526d431b9e7a
Ftosun21 <tosunfatih9.2@gmail.com>
2023-06-15 19:55:18 +0300

    added : testNG intro FirstTestNGTest

---
+++ src/test/java/com/kraftechnologie/tests/day07_testNG_intro/FirstTestNGTest.java
@@ -0,0 +1,21 @@
+package com.kraftechnologie.tests.day07_testNG_intro;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGTest {
+
+
+
+    @Test
+    public void test1(){
+        System.out.println("My First Test Case");
+
+    }
+    @Test
+    public void test2(){
+        System.out.println("My Second Test Case");
+
+    }
+
+
+}
