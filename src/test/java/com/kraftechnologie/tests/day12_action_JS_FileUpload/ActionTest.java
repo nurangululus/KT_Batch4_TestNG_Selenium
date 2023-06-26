fcb9be3ca0d5fb7a82cea45bf96d03a158f108b8
Ftosun21 <tosunfatih9.2@gmail.com>
2023-06-26 20:05:09 +0300

    added : hoverOver and dragAndDrop test

---
+++ src/test/java/com/kraftechnologie/tests/day12_action_JS_FileUpload/ActionTest.java
@@ -0,0 +1,71 @@
+package com.kraftechnologie.tests.day12_action_JS_FileUpload;
+
+import com.kraftechnologie.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ActionTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws InterruptedException {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test
+    public void hoverOver() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/hovers");
+
+        WebElement img2 = driver.findElement(By.xpath("(//img)[3]"));
+        //img2.click();
+        //Actions --> class that contains all the user interactions
+        Actions actions=new Actions(driver);
+        Thread.sleep(2000);
+
+        //moveToElement()--> move your mouse to the web element ( hover over)
+        //perform()--> perform the action, complete the action
+       actions.moveToElement(img2).perform();
+
+        WebElement view_profile1 = driver.findElement(By.linkText("View profile"));
+
+        System.out.println("view_profile1.getText() = " + view_profile1.getText());
+
+        Assert.assertTrue(view_profile1.isDisplayed(),"Verify that element is displayed");
+    }
+
+    @Test
+    public void dragAndDrop() {
+        driver.get("https://demoqa.com/droppable");
+
+        WebElement source = driver.findElement(By.id("draggable"));
+
+        WebElement target= driver.findElement(By.xpath("(//div[@id='droppable'])[1]"));
+
+        Actions actions=new Actions(driver);
+
+        actions.dragAndDrop(source,target).perform();
+
+        WebElement verifyMessage=driver.findElement(By.xpath("//p[text()='Dropped!']"));
+        System.out.println("verifyMessage.getText() = " + verifyMessage.getText());
+
+        Assert.assertTrue(verifyMessage.isDisplayed());
+
+        Assert.assertEquals(verifyMessage.getText(),"Dropped!","Verify that element has dropped");
+    }
+}
