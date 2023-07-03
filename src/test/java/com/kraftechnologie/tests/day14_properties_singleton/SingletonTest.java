0ad7012e56e8a01eedb34b7fbe4b2f291a6d11e5
Ftosun21 <tosunfatih9.2@gmail.com>
2023-07-03 22:08:08 +0300

    added : driver class test and testBase

--- src/test/java/com/kraftechnologie/tests/day14_properties_singleton/SingletonTest.java
+++ src/test/java/com/kraftechnologie/tests/day14_properties_singleton/SingletonTest.java
@@ -1,5 +1,7 @@
 package com.kraftechnologie.tests.day14_properties_singleton;
 
+import com.kraftechnologie.utilities.ConfigurationReader;
+import com.kraftechnologie.utilities.Driver;
 import com.kraftechnologie.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,22 +9,45 @@ import org.testng.annotations.Test;
 
 public class SingletonTest {
 
+//    @Test
+//    public void test1(){
+//        WebDriver driver1= WebDriverFactory.getDriver("chrome");//empty chrome browser
+//        driver1.get("https://www.amazon.com/");
+//        WebDriver driver2=WebDriverFactory.getDriver("chrome");
+//       // driver2.findElement(By.cssSelector("#twotabsearchtextbox"));
+//    }
+//
+//    @Test
+//    public void test2(){
+//       // Singleton singleton=new Singleton();
+//        String s1=Singleton.getInstance();
+//        String s2=Singleton.getInstance();
+//
+//        System.out.println("s1 = " + s1);
+//        System.out.println("s2 = " + s2);
+//
+//    }
+//
+//    @Test
+//    public void test3(){
+//       // WebDriver driver=WebDriverFactory.getDriver("chrome");
+//        WebDriver driver= Driver.get();
+//        driver.get("https://www.amazon.com/");
+//        driver.get(ConfigurationReader.get("url"));
+//    }
+
     @Test
-    public void test1(){
-        WebDriver driver1= WebDriverFactory.getDriver("chrome");
-        driver1.get("https://www.amazon.com/");
-        WebDriver driver2=WebDriverFactory.getDriver("chrome");
-       // driver2.findElement(By.cssSelector("#twotabsearchtextbox"));
+    public void test4(){
+       // WebDriver driver=WebDriverFactory.getDriver("chrome");
+        WebDriver driver= Driver.get();
+        driver.get("https://www.google.com/");
+        //Driver.get().get("https://www.google.com/");
+        Driver.closeDriver();
     }
-
     @Test
-    public void test2(){
-       // Singleton singleton=new Singleton();
-        String s1=Singleton.getInstance();
-        String s2=Singleton.getInstance();
-
-        System.out.println("s1 = " + s1);
-        System.out.println("s2 = " + s2);
-
+    public void test5(){
+       // WebDriver driver=WebDriverFactory.getDriver("chrome");
+        WebDriver driver= Driver.get();
+        driver.get("https://www.amazon.com/");
     }
 }
